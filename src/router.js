8f780ce88d8746b1af1b7022544d0f79fbee52af
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-15 13:26:07 +0100

    Created empty routed components

--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetails from './components/pages/coaches/CoachDetails.vue';
+import CoachList from './components/pages/coaches/CoachList.vue';
+import CoachRegistration from './components/pages/coaches/CoachRegistration.vue';
+import ContactCoach from './components/pages/requests/ContactCoach.vue';
+import ReceivedRequests from './components/pages/requests/ReceivedRequests.vue';
+import NotFound from './components/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: '/contact', component: null }] //Access to coach identifier
+      component: CoachDetails,
+      children: [{ path: '/contact', component: ContactCoach }] //Access to coach identifier
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', redirect: '/coaches' }
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: ReceivedRequests },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
 
