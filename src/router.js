d2a9fc6b65f2f66687b8c1702991f093bfbaa015
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-15 13:51:14 +0100

    Moved paged folder to correct location and updated imports

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory} from 'vue-router';
-import CoachDetails from './components/pages/coaches/CoachDetails.vue';
-import CoachList from './components/pages/coaches/CoachList.vue';
-import CoachRegistration from './components/pages/coaches/CoachRegistration.vue';
-import ContactCoach from './components/pages/requests/ContactCoach.vue';
-import ReceivedRequests from './components/pages/requests/ReceivedRequests.vue';
-import NotFound from './components/pages/NotFound.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import ReceivedRequests from './pages/requests/ReceivedRequests.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
