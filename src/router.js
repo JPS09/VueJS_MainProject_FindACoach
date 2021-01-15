3147481f184de307a6e212e2f6a240becc19832f
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-15 13:07:58 +0100

    Added Routes

--- src/router.js
+++ src/router.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: null },
+    {
+      path: '/coaches/:id',
+      component: null,
+      children: [{ path: '/contact', component: null }] //Access to coach identifier
+    },
+    { path: '/register', component: null },
+    { path: '/requests', component: null },
+    { path: '/:notFound(.*)', redirect: '/coaches' }
+  ]
 });
 
 export default router;
