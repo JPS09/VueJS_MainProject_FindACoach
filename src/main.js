eda1260cba68eaa7e7940324326207e0702e9839
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-15 15:30:01 +0100

    init main store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
-import App from './App.vue'
+import App from './App.vue';
 import router from './router.js';
+import store from './store/index.js';
 
 const app = createApp(App);
+
+app.user(store);
 app.use(router);
+
 app.mount('#app');
