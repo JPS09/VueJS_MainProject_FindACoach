0adf4042c735b34a8071b7b0540e7b84d7a15448
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-16 19:58:47 +0100

    Added provided css and imported form component into CoachRegistration

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,5 +1,17 @@
 <template>
- <section>
-   <h2>Join us as a Coach!</h2>
- </section>
+  <section>
+    <base-card>
+      <h2>Join us as a Coach!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm
+  }
+};
+</script>
