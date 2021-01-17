67c6d906d81368be11f38e0ef78e1353d00031d7
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-17 13:37:05 +0100

    Added validation methods and boolean to check

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -2,32 +2,42 @@
   <form @submit.prevent="submitForm">
     <div class="form-controls">
       <label for="firstname">First Name</label>
-      <input type="text" id="firstname" v-model.trim="firstName" />
+      <input type="text" id="firstname" v-model.trim="firstName.val" />
     </div>
     <div class="form-controls">
       <label for="lastname">Last Name</label>
-      <input type="text" id="lastname" v-model.trim="lastName" />
+      <input type="text" id="lastname" v-model.trim="lastName.val" />
     </div>
     <div class="form-controls">
       <label for="description">Description</label>
-      <textarea id="description" rows="5" v-model.trim="description" />
+      <textarea id="description" rows="5" v-model.trim="description.val" />
     </div>
     <div class="form-controls">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" v-model.number="rate" />
+      <input type="number" id="rate" v-model.number="rate.val" />
     </div>
     <div class="form-controls">
       <h3>What do you want to teach our students?</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <input
+          type="checkbox"
+          id="frontend"
+          value="frontend"
+          v-model="areas.val"
+        />
         <label for="frontend">Front-End Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <input
+          type="checkbox"
+          id="backend"
+          value="backend"
+          v-model="areas.val"
+        />
         <label for="backend">Back-End Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career" v-model="areas" />
+        <input type="checkbox" id="career" value="career" v-model="areas.val" />
         <label for="career">Career Counseling</label>
       </div>
     </div>
@@ -40,14 +50,46 @@ export default {
   emits: ['register-coach'],
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      rate: null,
-      description: '',
-      areas: []
+      firstName: {
+        val: '',
+        valid: true
+      },
+      lastName: {
+        val: '',
+        valid: true
+      },
+      rate: {
+        val: null,
+        valid: true
+      },
+      description: {
+        val: '',
+        valid: true
+      },
+      areas: {
+        val: [],
+        valid: true
+      },
+      formIsValid: true
     };
   },
   methods: {
+    validateForm() {
+      this.formIsValid = true;
+      this.firstName === ''
+        ? (this.firstName.valid = false)
+        : (this.firstName.valid = true);
+      this.lastName === ''
+        ? (this.lastName.valid = false)
+        : (this.lastName.valid = true);
+      this.rate === null ? (this.rate.valid = false) : (this.rate.valid = true);
+      this.description === ''
+        ? (this.description.valid = false)
+        : (this.description.valid = true);
+      this.areas.length === 0
+        ? (this.areas.valid = false)
+        : (this.areas.valid = true);
+    },
     submitForm() {
       const formData = {
         first: this.firstName,
