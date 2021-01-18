5e029c23f783e4cd18ba43e4cb7cee19b91140b8
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-18 15:24:54 +0100

    Added provided CSS

--- src/pages/requests/ReceivedRequests.vue
+++ src/pages/requests/ReceivedRequests.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    Requests
-  </div>
+  <section>
+    <base-card>
+      <header>
+        <h2>Received Requests</h2>
+      </header>
+      <ul></ul>
+      <h3>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
