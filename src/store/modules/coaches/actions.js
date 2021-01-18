c2c8d603a12659fe75eb1555af06543db0fa9295
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-18 18:12:39 +0100

    Added comments for clear understanding

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,5 +1,6 @@
 export default {
   async registerCoach(context, payload) {
+    // waits for await functions to finish
     const coachId = context.rootGetters.coachId;
     const coachData = {
       firstName: payload.first,
@@ -9,13 +10,14 @@ export default {
       areas: payload.areas
     };
     const response = await fetch(
+      //Is a promise, like a .then
       `https://seekacoach-56074-default-rtdb.europe-west1.firebasedatabase.app/coaches/coach${coachId}.json`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
       }
     );
-    const responseData = await response.json();
+    // const responseData = await response.json();
 
     if (!response.ok) {
       // show error
