1355cff5215e366e1060586ae23b7f8219de0776
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-18 20:56:01 +0100

    Implemented error handling and spinner

--- src/pages/requests/ReceivedRequests.vue
+++ src/pages/requests/ReceivedRequests.vue
@@ -1,10 +1,18 @@
 <template>
+  <base-dialog
+    :show="!!error"
+    title="Something went wrong"
+    @close="handleError"
+  >
+    {{ error }}
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Received Requests</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests">
         <request-item
           v-for="request in requests"
           :key="request.id"
@@ -20,6 +28,12 @@
 <script>
 import RequestItem from '../../components/requests/RequestItem.vue';
 export default {
+  data() {
+    return {
+      isLoading: false,
+      error: null
+    };
+  },
   components: {
     RequestItem
   },
@@ -28,8 +42,26 @@ export default {
       return this.$store.getters['requests/receivedRequests'];
     },
     hasRequests() {
-      return this.$store.getters['requests/hasRequests'];
+      return this.$store.getters['requests/hasRequests'] && !this.isLoading;
     }
+  },
+  methods: {
+    async loadRequest() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error =
+          error.message || 'Something went wrong while fetching requests';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    }
+  },
+  created() {
+    this.loadRequest();
   }
 };
 </script>
