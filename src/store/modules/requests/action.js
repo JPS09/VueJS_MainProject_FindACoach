111139aed363e4860b17995c34fa74f7800c3605
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-18 20:36:13 +0100

    Added sending HTTP request

--- src/store/modules/requests/action.js
+++ src/store/modules/requests/action.js
@@ -1,11 +1,30 @@
 export default {
-  addRequest(context, payload) {
+  async addRequest(context, payload) {
     const newRequest = {
-      id: new Date().toUTCString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message
     };
+
+    const response = await fetch(
+      `https://seekacoach-56074-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest)
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message ||
+          'Something went wrong while sending your request, please try again'
+      );
+      throw error;
+    }
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest);
   }
 };
