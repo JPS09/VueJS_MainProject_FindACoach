7b214f2e36d6050460bbc2fe839e153e20535983
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-18 13:24:49 +0100

    Added template into contactCoach

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    Please enter some text
-  </div>
+  <form @submit.prevent>
+    <div class="form-controls">
+      <label for="useremail">Your E-Mail</label>
+      <input type="email" id="useremail" />
+    </div>
+    <div class="form-controls">
+      <label for="usermessage">Your message to the coach</label>
+      <textarea name="message" id="usermessage" cols="20" rows="5"></textarea>
+    </div>
+    <div class="actions">
+      <base-button mode="outline">Send</base-button>
+    </div>
+  </form>
 </template>
