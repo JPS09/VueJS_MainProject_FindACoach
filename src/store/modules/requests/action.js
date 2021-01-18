127bf1d65bb8f6e28d446bb3a0c032cf715d9c1e
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-18 20:46:13 +0100

    Added get fetching of requests

--- src/store/modules/requests/action.js
+++ src/store/modules/requests/action.js
@@ -26,5 +26,31 @@ export default {
     newRequest.coachId = payload.coachId;
 
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.coachId;
+    const response = await fetch(
+      `https://seekacoach-56074-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message ||
+          'Something went wrong while fetching requests, please try again'
+      );
+      throw error;
+    }
+    const requests = [];
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message
+      };
+      requests.push(request);
+    }
+    context.commit('setRequests', requests);
   }
 };
