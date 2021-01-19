874368054575e1fb233ec34c34710ab08754c08d
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-19 20:49:12 +0100

    Added action to create a new user into DB

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,20 @@
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    const newUser = {
+      email: payload.email,
+      password: payload.password,
+      returnSecureToken: true
+    };
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDnPXkQ_Wc8-QHI7AuTXfwnKhNmMX6BaUA',
+      {
+        method: 'POST',
+        body: JSON.stringify(newUser)
+      }
+    );
+    if (!response.ok) {
+      //error handling
+    }
+  }
+};
