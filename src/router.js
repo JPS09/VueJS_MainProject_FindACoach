94cca628569ff8d95fdb00030d29e075453b1bf0
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-20 16:14:58 +0100

    Added global navigation guard

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import ReceivedRequests from './pages/requests/ReceivedRequests.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
+import store from './store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -18,11 +19,31 @@ const router = createRouter({
       props: true,
       children: [{ path: 'contact', component: ContactCoach }] //Access to coach identifier
     },
-    { path: '/register', component: CoachRegistration },
-    { path: '/requests', component: ReceivedRequests },
-    { path: '/login', component: UserAuth },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: { needsAuth: true }
+    },
+    {
+      path: '/requests',
+      meta: { needsAuth: true },
+      component: ReceivedRequests
+    },
+    { path: '/login', component: UserAuth, meta: { needDisconnected: true } },
     { path: '/:notFound(.*)', component: NotFound }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.needsAuth && !store.getters.isAuthenticated) {
+    //checks if user is logged in on routes that needs have auth
+    next('/login'); // Redirect to 'login' if that's not the case
+  } else if (to.meta.needDisconnected && store.getters.isAuthenticated) {
+    // checks if user is not logged in on the login route
+    next('/coaches'); // redirect to coaches if already logged
+  } else {
+    next(); // continues if everything is all right
+  }
 });
 
 export default router;
