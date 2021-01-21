1ae3db62a3e0cf38da33fa51a1d09fafd3356b08
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-21 17:03:37 +0100

    Made spinner and badge async since not used everywhere

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import router from './router.js';
 import store from './store/index.js';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
-import BaseBadge from './components/ui/BaseBadge.vue';
-import BaseSpinner from './components/ui/BaseSpinner.vue';
+// import BaseBadge from './components/ui/BaseBadge.vue';
+// import BaseSpinner from './components/ui/BaseSpinner.vue';
+// import BaseDialog from './components/ui/BaseDialog.vue'
 
 const BaseDialog = defineAsyncComponent(()=> import('./components/ui/BaseDialog.vue'))
+const BaseSpinner = defineAsyncComponent(()=> import('./components/ui/BaseSpinner.vue'))
+const BaseBadge = defineAsyncComponent(()=> import('./components/ui/BaseBadge.vue'))
 
 const app = createApp(App);
 
