27e24c70db0d425cd37aeee82911f51a42cfdbca
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-21 10:40:17 +0100

    Code Refactoring

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,17 +1,23 @@
 export default {
-  async logIn(context, payload) {
+  async auth(context, payload) {
+    const mode = payload.mode;
+
+    let endpointUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_AUTH_KEY}`;
+    if (mode === 'login') {
+      endpointUrl;
+    } else if (mode === 'signup') {
+      endpointUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_AUTH_KEY}`;
+    }
+
     const existingUser = {
       email: payload.email,
       password: payload.password,
       returnSecureToken: true
     };
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_AUTH_KEY}`,
-      {
-        method: 'POST',
-        body: JSON.stringify(existingUser)
-      }
-    );
+    const response = await fetch(endpointUrl, {
+      method: 'POST',
+      body: JSON.stringify(existingUser)
+    });
 
     const responseData = await response.json();
 
@@ -22,7 +28,7 @@ export default {
       );
       throw error;
     }
-    console.log(responseData);
+
     const localUser = {
       token: responseData.idToken,
       userId: responseData.localId,
@@ -31,36 +37,19 @@ export default {
 
     context.commit('setUser', localUser);
   },
-  async signUp(context, payload) {
-    const newUser = {
-      email: payload.email,
-      password: payload.password,
-      returnSecureToken: true
-    };
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_AUTH_KEY}`,
-      {
-        method: 'POST',
-        body: JSON.stringify(newUser)
-      }
-    );
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      const error = new Error(
-        responseData.message ||
-          'Something went wrong while creating your profile, please try again'
-      );
-      throw error;
-    }
-    console.log(responseData);
-    const localUser = {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
-    };
-    context.commit('setUser', localUser);
+  logIn(context, payload) {
+    // returning promises to components to allow usage of async await in methods
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
+    });
+  },
+  signUp(context, payload) {
+    // returning promises to components to allow usage of async await in methods
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    });
   },
   logOut(context) {
     const logOut = { token: null, userId: null, tokenExpiration: null };
