207045a580d142df968cc7d4e905dd8ebbfe3e6e
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-21 17:27:14 +0100

    Imported routed Component with the proper way and addec omments for informations(

--- src/router.js
+++ src/router.js
@@ -1,30 +1,32 @@
-import { defineAsyncComponent } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
-// import CoachDetails from './pages/coaches/CoachDetails.vue';
-// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-// import ContactCoach from './pages/requests/ContactCoach.vue';
-// import NotFound from './pages/NotFound.vue';
-// import ReceivedRequests from './pages/requests/ReceivedRequests.vue';
-import CoachList from './pages/coaches/CoachList.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import store from './store/index.js';
 
-const CoachDetails = defineAsyncComponent(() =>
-  import('./pages/coaches/CoachDetails.vue')
-);
-const CoachRegistration = defineAsyncComponent(() =>
-  import('./pages/coaches/CoachRegistration.vue')
-);
-const ContactCoach = defineAsyncComponent(() =>
-  import('./pages/requests/ContactCoach.vue')
-);
-const NotFound = defineAsyncComponent(() => import('./pages/NotFound.vue'));
-const ReceivedRequests = defineAsyncComponent(() =>
-  import('./pages/requests/ReceivedRequests.vue')
-);
+//This is the way to have Async Components in the router since the imports below are dynamic and supported by the router
+// ALL routes should be imported this way
 
-const router = createRouter({
+//Good
+  const CoachList = () => import('./pages/coaches/CoachList.vue')
+  const CoachDetails = () => import('./pages/coaches/CoachDetails.vue');
+  const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+  const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+  const NotFound = () => import('./pages/NotFound.vue');
+  const ReceivedRequests = () => import('./pages/requests/ReceivedRequests.vue');
+  const UserAuth = () => import('./pages/auth/UserAuth.vue');
+
+  //Bad
+  // const componentName = defineAsynComponent(()=>import('pathToFile'))
+
+  //Doesn't make use of the router support of dynamic imports
+  // import CoachDetails from './pages/coaches/CoachDetails.vue';
+  // import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+  // import ContactCoach from './pages/requests/ContactCoach.vue';
+  // import NotFound from './pages/NotFound.vue';
+  // import ReceivedRequests from './pages/requests/ReceivedRequests.vue';
+  // import UserAuth from './pages/auth/UserAuth.vue';
+  // import CoachList from './pages/coaches/CoachList.vue';
+
+  const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
