bbc4c6ac81e74b765c5094b778d3658bb9069bbd
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-21 16:54:17 +0100

    Moved UI to component folder and made baseDialog Async

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 import App from './App.vue';
 import router from './router.js';
 import store from './store/index.js';
-import BaseCard from './ui/BaseCard.vue';
-import BaseButton from './ui/BaseButton.vue';
-import BaseBadge from './ui/BaseBadge.vue';
-import BaseSpinner from './ui/BaseSpinner.vue';
-import BaseDialog from './ui/BaseDialog.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+
+const BaseDialog = defineAsyncComponent(()=> import('./components/ui/BaseDialog.vue'))
 
 const app = createApp(App);
 
